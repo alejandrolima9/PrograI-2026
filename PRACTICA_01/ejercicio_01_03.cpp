9207083d8c5d40f601df02d292d72a2f81aec54a
alejandrolima9 <alejandro.lima@ucb.edu.bo>
2026-02-18 00:28:53 -0400

    Add files via upload

---
+++ PRACTICA_01/ejercicio_01_03.cpp
@@ -0,0 +1,30 @@
+// Materia: Programación I, Paralelo 4
+
+// Autor: Alejandro Lima Iriarte
+
+// Carnet: 9873980
+
+// Carrera del estudiante: DISEÑO DIGITAL
+
+// Fecha creación: 16/02/2026
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int edad;
+char sexo;
+float estatura;
+
+cout<<"ingrese su edad: ";
+cin>>edad;
+cout<<"ingrese su sexo( M o F ): ";
+cin>>sexo;
+cout<<"ingrese su estatura: ";
+cin>>estatura;
+
+cout<<"su edad es: "<<edad<<endl<<"es de sexo: "<<sexo<<endl<<"usted mide: "<<estatura<<" m "<<endl;
+
+
+    return 0;
+ }
