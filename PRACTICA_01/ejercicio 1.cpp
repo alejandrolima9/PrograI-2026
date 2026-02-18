3b9f444df15b109409bc58525d2da7e92141880f
alejandrolima9 <alejandro.lima@ucb.edu.bo>
2026-02-17 21:21:44 -0400

    Add files via upload

---
+++ PRACTICA_01/ejercicio 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main(){
+cout<<"ingrese 2 numeros porfavor"<<endl;
+int num1;
+int num2;
+cin>>num1;
+cin>>num2;
+cout<<num1<<num2;
+
+
+    return 0;
+ }
